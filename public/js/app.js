d11c25f9b53eb44e3f64a1a49d44ae0a124d6a9c
Atte Pietikäinen <pietikainen.atte@gmail.com>
2015-10-15 14:20:10 +0300

    Setting up client side
    Added app.js and IO.js to client side along with necessary jquery
    library.

---
+++ public/js/app.js
@@ -0,0 +1,203 @@
+ var App = {
+
+   /**
+    * Keep track of the gameId, which is identical to the ID
+    * of the Socket.IO Room used for the players and host to communicate
+    *
+    */
+   gameId: 0,
+
+   /**
+    * This is used to differentiate between 'Host' and 'Player' browsers.
+    */
+   myRole: '',   // 'Player' or 'Host'
+
+   /**
+    * The Socket.IO socket object identifier. This is unique for
+    * each player and host. It is generated when the browser initially
+    * connects to the server when the page loads for the first time.
+    */
+   mySocketId: '',
+
+
+   /* *************************************
+    *                Setup                *
+    * *********************************** */
+
+   /**
+    * This runs when the page initially loads.
+    */
+   init: function () {
+      App.cacheElements();
+      App.showInitScreen();
+      App.bindEvents();
+
+   },
+
+   /**
+    * Create references to on-screen elements used throughout the game.
+    */
+   cacheElements: function () {
+      App.$doc = $(document);
+   },
+
+   /**
+    * Create some click handlers for the various buttons that appear on-screen.
+    */
+   bindEvents: function () {
+      // Builder (Host)
+      App.$doc.on('click', '#btnCreateGame', App.Host.onCreateClick);
+
+      // Player
+      App.$doc.on('click', '#btnJoinGame', App.Player.onJoinClick);
+      App.$doc.on('click', '#btnStart',App.Player.onPlayerStartClick);
+   },
+
+   /* *************************************
+    *             Game Logic              *
+    * *********************************** */
+
+   /**
+    * Show the initial PELABlocks Title Screen
+    * (with Start and Join buttons)
+    */
+   showInitScreen: function() {
+      //TODO: Implement after UI
+   },
+
+
+   /* *******************************
+    *         HOST CODE           *
+    ******************************* */
+   Host : {
+
+      /**
+       * Contains references to player data
+       */
+      players : [],
+
+      /**
+       * Flag to indicate if a new game is starting.
+       * This is used after the first game ends, and players initiate a new game
+       * without refreshing the browser windows.
+       */
+      isNewGame : false,
+
+      /**
+       * Keep track of the number of players that have joined the game.
+       */
+      numPlayersInRoom: 0,
+
+      /**
+       * Handler for the "Start" button on the Title Screen.
+       */
+      onCreateClick: function () {
+         // console.log('Clicked "Create A Game"');
+         IO.socket.emit('hostCreateNewGame');
+      },
+
+      /**
+       * The Host screen is displayed for the first time.
+       * @param data{{ gameId: int, mySocketId: * }}
+       */
+      gameInit: function (data) {
+         App.gameId = data.gameId;
+         App.mySocketId = data.mySocketId;
+         App.myRole = 'Host';
+         App.Host.numPlayersInRoom = 0;
+
+         App.Host.displayNewGameScreen();
+         // console.log("Game started with ID: " + App.gameId + ' by host: ' + App.mySocketId);
+      },
+
+      /**
+       * Show the Host screen containing the game URL and unique game ID
+       */
+      displayNewGameScreen : function() {
+         // Fill the game screen with the appropriate HTML
+         //TODO: Implement after the UI
+         // Display the URL on screen
+         //$('#gameURL').text(window.location.href);
+
+         // Show the gameId / room id on screen
+         $('#spanNewGameCode').text(App.gameId);
+      },
+
+      /**
+       * Update the Host screen when the first player joins
+       * @param data{{playerName: string}}
+       */
+      updateWaitingScreen: function(data) {
+         // Update host screen
+         $('#playersWaiting').text('Builder has joined the game.');
+
+      },
+
+      //TODO: Implementing gamelogic ralated host code
+   },
+   /* *****************************
+    *        PLAYER CODE        *
+    ***************************** */
+
+   Player : {
+
+      /**
+       * A reference to the socket ID of the Host
+       */
+      hostSocketId: '',
+
+      /**
+       * The player's name entered on the 'Join' screen.
+       */
+      myName: '',
+
+      /**
+       * Click handler for the 'JOIN' button
+       */
+      onJoinClick: function () {
+         //TODO: Implement after UI
+      },
+
+      /**
+       * The player entered their name and gameId (hopefully)
+       * and clicked Start.
+       */
+      onPlayerStartClick: function() {
+         // console.log('Player clicked "Start"');
+
+         // collect data to send to the server
+         var data = {
+            gameId : +($('#inputGameId').val()),
+            playerName : $('#inputPlayerName').val() || 'anon'
+         };
+
+         // Send the gameId and playerName to the server
+         IO.socket.emit('playerJoinGame', data);
+
+         // Set the appropriate properties for the current player.
+         App.myRole = 'Player';
+         App.Player.myName = data.playerName;
+      },
+
+      /**
+       * Display the waiting screen for player 1
+       * @param data
+       */
+      updateWaitingScreen : function(data) {
+        if(IO.socket.socket.sessionid === data.mySocketId){
+           App.myRole = 'Player';
+           App.gameId = data.gameId;
+
+           //TODO: Localization
+           $('#playerWaitingMessage').html('Please wait for instructor to start the game.');
+        }
+      },
+
+      //TODO: Implement gamelogic related builder code
+
+   },
+
+};
+
+IO.init();
+App.init();
